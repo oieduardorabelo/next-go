01374f33ecb083618480ec101041eee985a8fa54
Eduardo Rabelo <oieduardorabelo@gmail.com>
2017-10-12 16:28:59 +1300

    feat: adding timber for log

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const next = require('next')
+const timber = require('timber')
+
 const routes = require('./routes')
 
 const dev = process.env.NODE_ENV !== 'production'
@@ -7,9 +9,13 @@ const app = next({ dev })
 const handle = app.getRequestHandler()
 const handler = routes.getRequestHandler(app)
 
+const transport = new timber.transports.HTTPS(process.env.API_KEY_TIMER)
+timber.install(transport)
+
 app.prepare()
 .then(() => {
   const server = express()
+  server.use(timber.middlewares.express())
   server.use(handler)
 
   server.get('*', (req, res) => {
